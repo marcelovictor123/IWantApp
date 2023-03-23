b2d528e36f9314f184bda6b6b5e02c3a01e55e35
marcelo_victor <marcelo.victor525@gmail.com>
2023-03-22 22:14:56 -0300

    Adicionando Produto

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Infra.Data;
+
+public class ApplicationDbContext
+{
+    public DbSet<Product> Products { get; set; }    
+
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        
+
+    }
+}
